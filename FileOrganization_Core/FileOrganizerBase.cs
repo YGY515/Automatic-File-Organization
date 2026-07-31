096bc8c18d241984f42fd34e74d986e1aec4ec96
YGY 윤가영 <107830010+YGY515@users.noreply.github.com>
2026-07-31 11:44:32 +0900

    파일과 폴더 총합 변수 추가

--- FileOrganization_Core/FileOrganizerBase.cs
+++ FileOrganization_Core/FileOrganizerBase.cs
@@ -1,7 +1,10 @@
-﻿namespace FileOrganization_Core
+namespace FileOrganization_Core
 {
     public abstract class FileOrganizerBase
     {
+        public int FileCount { get; protected set; }    // 총 정리한 파일 
+        public int FolderCount { get; protected set; }  // 총 생성한 폴더
+
         public abstract string Organize(string path, CancellationToken token, IProgress<int> progress = null);
 
         public abstract void CollectFiles();
@@ -12,7 +15,7 @@
 
         public string PrintLog(int fileNum, int folderNum)
         {
-            return ($"파일 {fileNum}개를 정리하고 폴더 {folderNum}개를 생성했습니다.");
+            return ($"위치에서 파일 {fileNum}개를 정리하고 폴더 {folderNum}개를 생성했습니다.");
         }
     }
 }
