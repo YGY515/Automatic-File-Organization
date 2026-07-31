558f0048ff48c3730f43804c38947f56ded58307
YGY 윤가영 <107830010+YGY515@users.noreply.github.com>
2026-07-31 14:24:26 +0900

    DB에 로그를 기록하는 스크립트 업로드

---
+++ FileOrganization_Core/LogUploader.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http.Json;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FileOrganization_Core
+{
+    public class LogUploader
+    {
+        private static readonly HttpClient client = new HttpClient();
+
+        public static async Task SendLogAsync (string path, int organizeMethod, int fileCount, int folderCount, bool cancelled)
+        {
+            var log = new { Date = DateTime.Now, Path = path, OrganizeMethod = organizeMethod,
+                FileCount = fileCount, FolderCount = folderCount, WasCancelled = cancelled };
+
+            await client.PostAsJsonAsync("https://localhost:7179/api/logs", log);
+        }
+    }
+}
